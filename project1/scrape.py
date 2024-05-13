d913b6170e7dc7015d65ed66c391208a2cb98655
eddienubes <cool.gegeg@gmail.com>
2024-05-13 12:56:50 +0200

    feat(analysis): added correct prediction

--- project1/scrape.py
+++ project1/scrape.py
@@ -10,14 +10,18 @@ async def main() -> None:
     snp_500 = api.get_history('^GSPC')
     vix = api.get_history('^VIX')
     us_treasury_bonds = api.get_history('^TYX')
+    # Dow Jones Industrial Average
+    djia = api.get_history('^DJI')
 
     snp_path = os.path.join(os.getcwd(), 'project1', 'assets', 's&p500_max.csv')
     vix_path = os.path.join(os.getcwd(), 'project1', 'assets', 'vix_max.csv')
     us_treasury_bonds_path = os.path.join(os.getcwd(), 'project1', 'assets', 'us_bonds_max.csv')
+    djia_path = os.path.join(os.getcwd(), 'project1', 'assets', 'djia_max.csv')
 
     snp_500.to_csv(snp_path)
     vix.to_csv(vix_path)
     us_treasury_bonds.to_csv(us_treasury_bonds_path)
+    djia.to_csv(djia_path)
 
 
 asyncio.run(main())
